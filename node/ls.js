1a564c8d9be7951e7b8db258e43216b88668f600
Yulia Tenincheva <yulia.ten@dezigner.ru>
2014-11-08 11:57:57 +0200

    Some quality fix

--- node/ls.js
+++ node/ls.js
@@ -1,13 +1,12 @@
+'use strict';
+
 var fs = require('fs');
-var os = require('os');
-
 var path = '../data/snippets';
-
 var folders = fs.readdirSync(path);
 var files = {};
 
 folders.forEach(function (folder) {
-    files[folder] = fs.readdirSync(path+'/'+folder);
+    files[folder] = fs.readdirSync(path + '/' + folder);
 });
 
 console.log(files);
