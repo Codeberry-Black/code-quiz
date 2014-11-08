afbbbee87cf24dfcbfb9c52928d7cfe68b7d4680
Yulia Tenincheva <yulia.ten@dezigner.ru>
2014-11-08 11:55:43 +0200

    Read files with NodeJS script

--- node/ls.js
+++ node/ls.js
@@ -1,3 +1,13 @@
+var fs = require('fs');
+var os = require('os');
+
 var path = '../data/snippets';
 
-// TODO list this folder
+var folders = fs.readdirSync(path);
+var files = {};
+
+folders.forEach(function (folder) {
+    files[folder] = fs.readdirSync(path+'/'+folder);
+});
+
+console.log(files);
