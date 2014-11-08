0b45220d01ed036cb0146f67b1cca4171c8e2c38
Yulia Tenincheva <yulia.ten@dezigner.ru>
2014-11-08 15:07:44 +0200

    Count number of snippets per language

--- node/ls.js
+++ node/ls.js
@@ -3,19 +3,28 @@
 var fs = require('fs'),
     os = require('os'),
     dataPath = '../data/snippets',
-    folders = fs.readdirSync(path),
+    folders = fs.readdirSync(dataPath),
+    countedSnippets = {},
     files = {};
 
 folders.forEach(function (folder) {
-    files[folder] = fs.readdirSync(dataPath + '/' + folder);
+    var filesInDir = fs.readdirSync(dataPath + '/' + folder);
+    countedSnippets[folder] = filesInDir.length;
+    files[folder] = filesInDir;
 });
 
 fs.writeFile('snippets.json', JSON.stringify(files, os.EOL, ' '), function (err) {
     if (err) {
         console.log(err);
     } else {
-        console.log("The file was saved!");
+        console.log("snippets.json is updated");
     }
 });
 
-console.log(files);
+fs.writeFile('countedSnippets.json', JSON.stringify(countedSnippets, os.EOL, ' '), function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("countedSnippers.json is updated");
+    }
+});
