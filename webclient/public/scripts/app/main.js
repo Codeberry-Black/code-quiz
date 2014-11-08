a81cebbb47c90a4f7935f5cefd76e704130974d4
Yulia Tenincheva <yulia.ten@dezigner.ru>
2014-11-09 01:39:39 +0200

    Try-fix :boom:

--- webclient/public/scripts/app/main.js
+++ webclient/public/scripts/app/main.js
@@ -1,6 +1,8 @@
 var app = (function () {
     'use strict';
 
+    var port = 12346;
+
     // global error handling
     var showAlert = function(message, title, callback) {
         navigator.notification.alert(message, callback || function () {
@@ -67,7 +69,7 @@ var app = (function () {
         var currentUser = kendo.observable({ data: null });
         var usersData;
         var loadGames = function () {
-            return //TODO load from server el.Users.currentUser()
+            return  //TODO load from server el.Users.currentUser()
             .then(function (data) {
                 var currentUserData = data.result;
                 //currentUserData.PictureUrl = AppHelper.resolveProfilePictureUrl(currentUserData.Picture);
@@ -121,11 +123,66 @@ var app = (function () {
             loginWithFacebook: loginWithFacebook
         };
     }());
-    
+
+    // signup view model
+    var singupViewModel = (function () {
+        var dataSource;
+        var signup = function () {
+            dataSource.Gender = parseInt(dataSource.Gender);
+            var birthDate = new Date(dataSource.BirthDate);
+            if (birthDate.toJSON() === null)
+                birthDate = new Date();
+            dataSource.BirthDate = birthDate;
+            Everlive.$.Users.register(
+                dataSource.Username,
+                dataSource.Password,
+                dataSource)
+            .then(function () {
+                showAlert("Registration successful");
+                mobileApp.navigate('#welcome');
+            },
+                  function (err) {
+                      showError(err.message);
+                  }
+            );
+        };
+        var show = function () {
+            dataSource = kendo.observable({
+                Username: '',
+                Password: '',
+                DisplayName: '',
+                Email: '',
+                Gender: '1',
+                About: '',
+                Friends: [],
+                BirthDate: new Date()
+            });
+            kendo.bind($('#signup-form'), dataSource, kendo.mobile.ui);
+        };
+        return {
+            show: show,
+            signup: signup
+        };
+    }());
+
+    var apiCall = function (method, params, cb) {
+        $.getJSON('localhost:' + port, params, function(result){
+            if(result.result !== 0){
+                console.assert(result, 'Stupid error message');
+            } else {
+                cb(result);
+            }
+        });
+    };
+
     return {
         viewModels: {
             login: loginViewModel,
-            gamesModel: gamesModel
+            gamesModel: gamesModel,
+            signup: singupViewModel,
+            usersModel: usersModel,
+            playingGame: playingGameViewModel,
+            apiCall: apiCall
         },
         mobileApp: mobileApp,
         joinToGame: joinToGame
