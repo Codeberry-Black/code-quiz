6edee5a1e28706fbcc77c07f8d703a30cbe2d1cd
Hristo Georgiev <bateto_pro@abv.bg>
2014-11-08 15:25:57 +0200

    Stable version of collector.py (run code, run)

--- mining/collector.py
+++ mining/collector.py
@@ -4,41 +4,131 @@ from StringIO import StringIO
 import gzip
 import json
 import os
+import codecs
+import threading
+from random import randrange
+from time import sleep
 
 
 class Manager():
-    def __init__(self):
-        self.atags = {}
-        self.aids = {}
-        #self.answer_lock
+    def __init__(self, tags_writer):
+        self.tags_writer = tags_writer
+        self.read_lock = threading.Lock()
+        self.write_lock = threading.Lock()
+        self.tagscnt = 0
+        self.tagslimit = 1000
+
+        self.types = ['answer', 'question']
+        self.tags = {'answer': {}, 'question': {}}
+        self.body = {'answer': [], 'question': []}
+        self.passed = {'answer': set(), 'question': set()}
+
+    def add_entry(self, ID, body, tags, etype):
+        self.write_lock.acquire(True)
+
+        try:
+            if not ID in self.passed[etype]:
+                self.passed[etype].add(ID)
+                self.body[etype].append((ID, body))
+
+                #print etype,  self.body[etype].__len__()
+                for tag in tags:
+                    if not tag in self.tags[etype]:
+                        self.tags[etype][tag] = []
+                    self.tags[etype][tag].append(ID)
+                    self.tagscnt = self.tagscnt + 1
+
+            if self.tagslimit <= self.tagscnt:
+                self.flush_tags(False)
+
+        finally:
+            self.write_lock.release()
+
+    def get_entry(self, etype):
+        self.read_lock.acquire(True)
+
+        try:
+            if self.body[etype].__len__() > 0:
+                return self.body[etype].pop(0)
+            else:
+                return False
+        finally:
+            self.read_lock.release()
+
+    def flush_tags(self, acquire):
+        if acquire:
+            self.write_lock.acquire(True)
+
+        print('Flush tags')
+
+        for etype in self.types:
+            for tag in self.tags[etype]:
+                self.tags_writer.write(tag, self.tags[etype][tag], etype)
+            self.tags[etype] = {}
+
+        if acquire:
+            self.write_lock.release()
 
 
+class SnippetWriter(threading.Thread):
+    def __init__(self, ID, body, folder, lock):
+        threading.Thread.__init__(self)
+        self.daemon = True
 
-        self.qtags = {}
-        self.qids = {}
-
-    def add_question(self, ID, body, tags):
-        print(id)
-
-    def add_answer(self, ID, body, tags):
-        print(id)
-
-
-
-
-class SnippetWriter():
-    def __init__(self):
         self.path = '.'
+        self.folder = folder
+        self.body = body
+        self.ID = ID
+        self.lock = lock
 
-    def write(self, ID, body, folder):
-        folder = os.path.join(self.path, folder)
 
-        if not os.path.exists(folder):
-            os.makedirs(folder)
+    def run(self):
+        self.lock.acquire(True)
 
-        out = open(os.path.join(folder, ID.__str__()), "w")
-        out.write(body)
-        out.close()
+        print(self.folder + '  ' + self.ID.__str__())
+
+        try:
+            folder = os.path.join(self.path, self.folder)
+
+            if not os.path.exists(folder):
+                os.makedirs(folder)
+
+            out = codecs.open(os.path.join(folder, self.ID.__str__() + '.html'), "w", "utf-8")
+            out.write(self.body)
+            out.close()
+        finally:
+            self.lock.release()
+
+
+class SnippetRunner():
+    def __init__(self, manager, readers):
+        self.manager = manager
+        self.readers = readers
+        self.types = ['answer', 'question']
+        self.locks = []
+        for i in range(readers):
+            self.locks.append(threading.Lock())
+
+    def run(self):
+        n = 1
+        while True:
+            for i in range(self.readers):
+                r = self.locks[i].acquire(False)
+                if not r:
+                    n += 1
+                else:
+                    self.locks[i].release()
+
+                    idx = randrange(2)
+
+                    entry = self.manager.get_entry(self.types[idx])
+                    if not entry: continue
+
+                    w = SnippetWriter(entry[0], entry[1], self.types[idx], self.locks[i])
+                    w.start()
+
+            n = 0
+            sleep(0.1)
 
 
 class TagsWriter():
@@ -47,16 +137,19 @@ class TagsWriter():
         if not os.path.exists(self.path):
             os.makedirs(self.path)
 
-    def write(self, name, ids):
+    def write(self, name, ids, prefix):
         out = open(os.path.join(self.path, name), "a")
 
         for k, v in enumerate(ids):
-            out.write(v.__str__() + "\n")
+            out.write( '/' + prefix + '/' + v.__str__() + ".html\n")
         out.close()
 
-class PageLoader():
+
+class PageLoader(threading.Thread):
     def download(self):
         query = urllib.urlencode(self.params)
+        print(urllib.urlencode(self.params))
+
         url = self.baseurl + '?' + query
 
         request = urllib2.Request(url)
@@ -69,33 +162,48 @@ class PageLoader():
         else:
             raw = response.read()
 
-        return  json.loads(raw)
+        data = json.loads(raw)
+
+        for question in data[u'items']:
+            self.manager.add_entry(question[u'question_id'], question[u'body'], question[u'tags'], 'question')
+            if u'answers' in question and question[u'answers'].__len__() > 0:
+                for answer in question[u'answers']:
+                    self.manager.add_entry(answer[u'answer_id'], answer[u'body'], question[u'tags'], 'answer')
 
     def __init__(self, params, manager):
+        threading.Thread.__init__(self)
+        self.daemon = True
+
         self.baseurl = 'https://api.stackexchange.com/2.2/questions'
 
         params['page'] = 1
         params['site'] = 'stackoverflow'
         params['filter'] = '!gB66oJbwvbd250ho0PQ7KOfUSej(N37(_xy'
+        params['key'] = 'LxeP1AMviWrz1ukID*JN*g(('
 
         self.params = params
         self.manager = manager
 
-
-manager = Manager()
-
-newest = PageLoader({'order': 'desc', 'sort': 'creation'}, manager)
-oldest = PageLoader({'order': 'asc', 'sort': 'creation'}, manager)
-most_active = PageLoader({'order': 'desc', 'sort': 'votes'}, manager)
-#less_active = PageLoader({'order': 'asc', 'sort': 'votes'}, manager)
+    def run(self):
+        page = 1
+        while True:
+            self.params['page'] = page
+            page = page + 1
+            self.download()
 
 
 t = TagsWriter()
-t.write('test', [1,2,3,4,5,6])
-t.write('test', [1,2,3,4,5,6])
+manager = Manager(t)
 
 
+loaders = {
+    'newest': PageLoader({'order': 'desc', 'sort': 'creation'}, manager),
+    'oldest': PageLoader({'order': 'asc', 'sort': 'creation'}, manager),
+    'most_active': PageLoader({'order': 'desc', 'sort': 'votes'}, manager)
+}
 
-w = SnippetWriter()
-w.write(1, 'Test', 'questions')
-w.write(1, 'Test', 'answers')
+for k in loaders:
+    loaders[k].start()
+
+runner = SnippetRunner(manager, 10)
+runner.run()
