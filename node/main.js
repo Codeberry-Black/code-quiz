4d5410c10ded5b21cccb20ff1741554a6a196e49
YordanL <yordanlyudmilov@gmail.com>
2014-11-08 23:48:05 +0200

    current state

--- node/main.js
+++ node/main.js
@@ -12,10 +12,63 @@ var get_json = function(name, callback){
   });
 };
 
+var get_snippet = function(lang, id){
+	var filePath = path.normalize('../data/snippets/'+lang+"/"+id);
+	fs.readFile(filePath, 'utf8', function (err, data) {
+    if (err) {console.log(err);}
+    callback(data);
+  });
+};
+
 var langs = {},
     games = {}
 
 ;
+
+var shuffle = function shuffle(o){
+    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+};
+
+var get_game_set = function(count, ansCount){
+	// Get random snippets
+	var selectedSnippets = [];
+	var elementsSelected = 0;
+	var keys = Object.keys(langs);
+	var slugs = {};
+	
+	while(elementsSelected != 3){
+		var selectedLang = keys[Math.floor(Math.random() * keys.length)];
+		if(langs[selectedLang].data != 0){
+			var tempKeys = Object.keys(langs[selectedLang].data);
+			var id = langs[selectedLang].data[tempKeys[Math.floor(Math.random() * tempKeys.length)]];
+			var slug = selectedLang + id;
+			if(slugs[slug])
+				continue;
+			slugs[slug] = true;
+			
+			
+			// Get ansCount answers, one of them right
+			var answers = [selectedLang];
+			for(var i = 0; i < ansCount - 1; i++){
+				var newAnswer;
+				do{
+					newAnswer = keys[Math.floor(Math.random() * keys.length)];
+				}while(answers.lastIndexOf(newAnswer) != -1);
+				answers.push(newAnswer);
+			}
+			
+			shuffle(answers);
+			selectedSnippets.push( {lang: selectedLang, id: id, answers: answers} );
+			elementsSelected++;
+		}	
+	}
+	
+	
+	
+	return selectedSnippets;
+};
+
 var init = function(){
   get_json('langs', function(ret){
     for (var k in ret){
@@ -25,12 +78,11 @@ var init = function(){
       for (var k in ret){
         langs[k].data = ret[k];
       }
-      get_json('games', function(ret){
-        
-        
-        console.log(ret);
-      });
-      
+	  
+	  
+	  
+	  console.log(get_game_set(3, 4));
+	  
       // GET USERS
       // GET GAMES
       // START SERVER
