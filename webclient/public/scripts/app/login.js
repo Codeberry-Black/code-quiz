d7071f345cad8e559d6c6e2cb34d646c4165d681
Porter <Drgreen_bg@yahoo.com>
2014-11-09 00:00:51 +0200

    FB login

---
+++ webclient/public/scripts/app/login.js
@@ -0,0 +1,36 @@
+window.fbAsyncInit = function () {
+    FB.init({
+        appId: '733423956732622',
+        status: true, // check login status
+        cookie: true, // enable cookies to allow the server to access the session
+        xfbml: true  // parse XFBML
+    });
+
+    FB.login(function (response) {
+        if (response.authResponse) {
+            getProfileInfo();
+        } else {
+            console.log('User cancelled login or did not fully authorize.');
+        }
+    }, { scope: 'read_friendlists,user_photos, user_birthday' });
+};
+
+// Load the SDK asynchronously
+(function (d) {
+    var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+    if (d.getElementById(id)) { return; }
+    js = d.createElement('script'); js.id = id; js.async = true;
+    js.src = "//connect.facebook.net/en_US/all.js";
+    ref.parentNode.insertBefore(js, ref);
+}(document));
+
+
+function getProfileInfo() {
+    FB.api('/me', function (response) {
+        var holder = $("#profile-info");
+        var name = response.name;
+        var url = "https://graph.facebook.com/" + response.id + "/picture";
+        holder.append("<img src =" + url + "/><h1 id='greetings'>" + name + "</h1>");
+    });
+    $("#log").css("display", "none");
+}
